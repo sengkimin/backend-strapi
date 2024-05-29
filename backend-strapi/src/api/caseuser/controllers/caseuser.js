b06f43112e3daaf66646c31b311c6406133233a3
sengkimin <sengin.pse@gmail.com>
2024-05-29 11:20:10 +0700

    add more

---
+++ backend-strapi/src/api/caseuser/controllers/caseuser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * caseuser controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::caseuser.caseuser');
---
+++ backend-strapi/src/api/caseuser/routes/caseuser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * caseuser router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::caseuser.caseuser');
